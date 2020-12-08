57364c9afad493105cdf151c587dc7e1849a8cae
rzeydelis <bobbyzeydelis@gmail.com>
2020-12-07 19:24:20 -0500

    add flask app python file

---
+++ app.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec 13 10:49:02 2019
+
+@author: Bobby
+"""
+
+# importing flask module fro
+from flask import Flask, render_template,request
+import delete
+
+# initializing a variable of Flask
+app = Flask(__name__)
+
+
+# home page where you can click where you want to go on the web page
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+# front page - dedication to kriti for being supporting
+@app.route('/kriti')
+def kriti():
+    return render_template('kriti.html')
+
+# fill out the address and zip code to return information
+@app.route('/form')
+def index():
+    return render_template('form.html')
+
+@app.route('/output',  methods=['POST'])
+def success():
+    if request.method == 'POST':
+        #email = request.form['email']
+        #return render_template('success.html', email=email)
+        address = request.form['address']
+        zipcode = request.form['zipcode']
+        zestimate, rent_zestimate, home_size, num_of_bedrooms, num_of_bathrooms, property_size, year_built = delete.zillow_info(address, zipcode)
+        
+        
+        return render_template('success.html',
+                               zestimate = zestimate,
+                               rent_zestimate = rent_zestimate,
+                               home_size = home_size,
+                               num_of_bedrooms = num_of_bedrooms,
+                               num_of_bathrooms = num_of_bathrooms,
+                               property_size = property_size,
+                               year_built = year_built).format()
+    else:
+        pass
+ 
+@app.route('/random_output', methods=['POST'])
+def random_output():
+    if request.method == 'POST':
+        rent_zestimate, full_address, home_size, last_sold_price, last_sold_date = delete.random_address()
+        
+        return render_template('random_success.html',
+                               rent_zestimate = rent_zestimate,
+                               full_address = full_address,
+                               home_size = home_size,
+                               last_sold_price = last_sold_price,
+                               last_sold_date = last_sold_date).format()
+
+if __name__ == "__main__":
+    app.run()
